798bc6963c99ffcd1aaa3f94ae96fd0d2ebd9411
Karamelika22 <nasteapopovichi@gmail.com>
2023-10-28 02:24:10 -0700

    Added constructs and operators

--- Code/Enemy.cpp
+++ Code/Enemy.cpp
@@ -1,6 +1,45 @@
 #include "Enemy.h"
 #include "Board.h"
 #include <cstdlib>
+#include <iostream>
+
+Enemy::Enemy() {
+    x = 0;
+    y = 0;
+    dir = 0;
+    delay = 1;
+    count_delay = 0;
+}
+
+Enemy::Enemy(int startX, int startY, int startDir, int startDelay, int startCountDelay) {
+    x = startX;
+    y = startY;
+    dir = startDir;
+    delay = startDelay;
+    count_delay = startCountDelay;
+}
+
+Enemy::Enemy(const Enemy& other) {
+    x = other.x;
+    y = other.y;
+    dir = other.dir;
+    delay = other.delay;
+    count_delay = other.count_delay;
+}
+
+bool Enemy::operator==(const Enemy& other) const {
+    return (x == other.x && y == other.y && dir == other.dir && delay == other.delay && count_delay == other.count_delay);
+}
+
+std::istream& operator>>(std::istream& is, Enemy& enemy) {
+    is >> enemy.x >> enemy.y >> enemy.dir >> enemy.delay >> enemy.count_delay;
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Enemy& enemy) {
+    os << "x: " << enemy.x << ", y: " << enemy.y << ", dir: " << enemy.dir << ", delay: " << enemy.delay << ", count_delay: " << enemy.count_delay;
+    return os;
+}
 
 void Enemy::drwa() {
     map[y][x] = 5;
--- Code/Enemy.h
+++ Code/Enemy.h
@@ -8,6 +8,11 @@ public:
     int delay;
     int count_delay;
 
+    Enemy();
+    Enemy(int, int, int, int, int);
+    Enemy(const Enemy& other);
+    bool operator==(const Enemy& other) const;
+
     void drwa();
     void move();
     bool collision(int fx, int fy);
